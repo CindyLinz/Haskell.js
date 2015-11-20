3c2bede973924828927c04c01fe498cb908bceea
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2015-11-20 18:06:05 +0800

    把 show_int 的 top-level define 去掉

--- trans/sample/show_int.hs
+++ trans/sample/show_int.hs
@@ -1,29 +1,31 @@
-showPosInt = \n ->
+main =
   let
-    showDigit = \d -> case d of
-      0 -> '0'
-      1 -> '1'
-      2 -> '2'
-      3 -> '3'
-      4 -> '4'
-      5 -> '5'
-      6 -> '6'
-      7 -> '7'
-      8 -> '8'
-      9 -> '9'
-
-    go = \n -> case n of
-      0 -> []
-      _ -> (:) (showDigit (mod n 10)) (go (div n 10))
-
-    reverse = \ls ->
+    showPosInt = \n ->
       let
-        go = \ls rs -> case ls of
-          [] -> rs
-          (:) a as -> go as ((:) a rs)
-      in
-        go ls []
-  in
-    reverse (go n)
+        showDigit = \d -> case d of
+          0 -> '0'
+          1 -> '1'
+          2 -> '2'
+          3 -> '3'
+          4 -> '4'
+          5 -> '5'
+          6 -> '6'
+          7 -> '7'
+          8 -> '8'
+          9 -> '9'
 
-main = putStrLn (showPosInt 257)
+        go = \n -> case n of
+          0 -> []
+          _ -> (:) (showDigit (mod n 10)) (go (div n 10))
+
+        reverse = \ls ->
+          let
+            go = \ls rs -> case ls of
+              [] -> rs
+              (:) a as -> go as ((:) a rs)
+          in
+            go ls []
+      in
+        reverse (go n)
+  in
+    putStrLn (showPosInt 257)
