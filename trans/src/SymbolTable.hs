ccd4396f2fe0cd00cdb57b0575311800f9cf4122
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2016-05-26 23:03:24 +0800

    draft

--- trans/src/SymbolTable.hs
+++ trans/src/SymbolTable.hs
@@ -3,17 +3,38 @@ module SymbolTable
   ) where
 
 import Data.Functor
+import qualified Data.Map.Strict as M
+import Lens.Micro
 
 import Language.Haskell.Exts.Annotated.Syntax
 
 import SymbolTableData
 import DesugarClass
 
+lookupSymbolTableLayer :: SymbolTableLayer -> String -> SymbolQueryResult
+lookupSymbolTableLayer syml name =
+  maybe SymbolQueryNotFound id (M.lookup name syml)
+
+lookupSymbolTable :: SymbolTable -> Maybe String -> String -> SymbolQueryResult
+lookupSymbolTable symt Nothing name = localQuery (symtLocal symt) where
+  localQuery [] = SymbolQueryNotFound
+  localQuery (syml : symls) = case lookupSymbolTableLayer syml name of
+    SymbolQueryNotFound -> localQuery symls
+    others -> others
+
 addSymbolTableLayer :: SymbolTable -> SymbolTable
-addSymbolTableLayer = undefined
+addSymbolTableLayer symt = symt
+  {symtLocal = emptySymbolTableLayer : symtLocal symt}
+
+addLocalSymbolToLayer :: SymbolProperty -> SymbolTableLayer -> SymbolTableLayer
+addLocalSymbolToLayer prop syml = undefined
+
+addLocalSymbolToTable :: SymbolProperty -> SymbolTable -> SymbolTable
+addLocalSymbolToTable = undefined
+--addLocalSymbolToTable prop symt = symt {symtLocal = head (symtLocal symt)
 
 addLocalSymbol :: SymbolProperty -> SymbolTable -> SymbolTable
-addLocalSymbol = undefined
+addLocalSymbol = addLocalSymbolToTable
 
 importSymbols :: ImportDecl l -> SymbolTable -> SymbolTable -> SymbolTable
 importSymbols = undefined
--- trans/src/SymbolTableData.hs
+++ trans/src/SymbolTableData.hs
@@ -1,16 +1,35 @@
 module SymbolTableData where
 
+import qualified Data.Map.Strict as M
+import Lens.Micro.Type
+
 import Language.Haskell.Exts.Annotated.Syntax
 import Language.Haskell.Exts.Pretty
 import Language.Haskell.Exts.SrcLoc
 
 data SymbolProperty = SymbolProperty
-  { sympName :: !(QName ())
+  { sympName :: !(QName SrcSpan)
   , sympPrio :: {-# UNPACKED #-} !Int -- infix operator priority
-  , sympAssoc :: !(Assoc ())
+  , sympAssoc :: !(Assoc SrcSpan)
   }
 
-type SymbolTable = SrcInfo l => QName l -> Either String SymbolProperty
+data SymbolQueryResult
+  = SymbolQuerySuccess !SymbolProperty
+  | SymbolQueryDup [SrcSpan]
+  | SymbolQueryNotFound
 
+type SymbolTableLayer = M.Map String SymbolQueryResult
+emptySymbolTableLayer :: SymbolTableLayer
+emptySymbolTableLayer = M.empty
+
+data SymbolTable = SymbolTable
+  { symtExternal :: M.Map String SymbolTableLayer
+  , symtLocal :: [SymbolTableLayer]
+  }
 emptySymbolTable :: SymbolTable
-emptySymbolTable name = Left ("symbol " ++ prettyPrint name ++ " not found.")
+emptySymbolTable = SymbolTable
+  { symtExternal = M.empty
+  , symtLocal = []
+  }
+symtExternalL :: Lens' SymbolTable (M.Map String SymbolTableLayer)
+symtExternalL 
